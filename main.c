d5271d8b422c0e5b86b0ba6edd962b4c853776f6
Spencer Neukam <>
2021-04-27 20:46:22 +0000

    parsed_input() correctly handles putting command in format of "/bin/<cmd>" into first argument[0]

--- main.c
+++ main.c
@@ -1,4 +1,27 @@
-// #include "headers.h"
+/*
+  Assignment 3 - Small Shell (Portfolio)
+  CS 344 Operating Systems
+  Spencer Neukam
+  5/3/2021
+
+  This program mimicks a shell. The commands cd, exit, and status are built in.
+  All other commands are passed to the exec family of functions.
+
+  Supported Features
+  - blank lines
+  - comment lines (#example comment)
+  - exit
+  - cd
+  - status (returns the... )
+  - all other standard commands
+  - up to 512 arguments for one command
+  - input and output redirection
+  - using & at the end of a command to run a process in the background
+  
+  Unsupported
+  - midline comments
+  - probably a lot of other stuff.
+*/
 
 #include <fcntl.h>
 #include <stdio.h>
@@ -11,13 +34,27 @@
 #include <limits.h>
 #include <time.h>
 #include <stdbool.h>
+#include <sys/wait.h>
+#include <stdio.h>
 
 #define MAX_COMMAND_LEN 2048
 #define MAX_ARGUMENTS 512
-#define EXIT "exit"
-#define CD "cd"
-#define STATUS "status"
 #define PID_INT_LENGTH 12
+#define MAX_BACKGROUND_PIDS 50
+
+// Global struct to track process ids of children (background processes)
+/*
+struct pids
+{
+  int length;
+  char *pids[MAX_BACKGROUND_PIDS];
+};
+char *background_pids[];
+int l;
+
+for (l=0; l<MAX_BACKGROUND_PIDS; l++){
+
+}*/
 
 
 struct command
@@ -30,6 +67,20 @@ struct command
     bool background_process;
     char *freetext;
 };
+
+/* 
+  Function arraylen()
+  ---------------------------------
+  description:
+    returns the length of an array
+  arg arr: pointer to an array
+  returns: int
+*/
+/*
+int arraylen(char *arr) {
+  int i = 0;
+  while (true)
+}*/
 
 /* 
   Function printf_custom()
@@ -105,7 +156,6 @@ void copytoken(char *place_here, char *token)
   else if (strcmp(newline, "\n") == 0){
     strncpy(place_here, token, strlen(token) - 1);
   }
-  printf("placed_here: %s\n", place_here);
   return;
 }
 
@@ -119,6 +169,9 @@ void copytoken(char *place_here, char *token)
 */
 struct command *parse_input(char *input)
 {
+
+  // TODO: CLEAN UP THIS CODE AND MAYBE SPLIT UP INTO SUPPORTING FUNCTIONS.
+
   // For use with strtok_r to tokenize input
   char *saveptr;
   char *token;
@@ -141,15 +194,18 @@ struct command *parse_input(char *input)
   int i;
   for (i=0;i<MAX_ARGUMENTS;i++){
       parsed_input->arguments[i] = NULL;
-  }  
+  }
 
   int j;
   for (j=0;j<MAX_ARGUMENTS; j++){
     
-    // get the correct token (the next string in the user input)
+    // get the correct token
     if(j < 1){
       token = strtok_r(input, " ", &saveptr);
       copytoken(parsed_input->command, token);
+
+      parsed_input->arguments[0] = calloc(strlen(token)+6, sizeof(char));
+      sprintf(parsed_input->arguments[0], "/bin/%s", parsed_input->command);
     }
     else {
       token = strtok_r(NULL, " ", &saveptr);
@@ -166,8 +222,10 @@ struct command *parse_input(char *input)
       break;
     }
 
-    parsed_input->arguments[j] = calloc(strlen(token)+1, sizeof(char));
-    copytoken(parsed_input->arguments[j], token);
+    if (j > 0){
+      parsed_input->arguments[j] = calloc(strlen(token)+1, sizeof(char));
+      copytoken(parsed_input->arguments[j], token);
+    }
   }
 
   // handle echo command
@@ -199,17 +257,15 @@ struct command *parse_input(char *input)
 */
 int is_comment(struct command *parsed_input)
 {
-  char *saveptr;
-  char *token;
-  char *ret;
+  char *comment;
   int is_comment;
   char *temp_full_input = calloc(MAX_INPUT+1, sizeof(char));
 
   strcpy(temp_full_input, parsed_input->full_text);  
-  ret = strstr(temp_full_input, "#");
+  comment = strstr(temp_full_input, "#");
   
-  if (ret != NULL){
-    if (ret - temp_full_input == 0) {
+  if (comment != NULL){
+    if (comment - temp_full_input == 0) {
       is_comment = 1;
     }
     else {
@@ -229,6 +285,7 @@ int is_comment(struct command *parsed_input)
     cd -> changes current working directory to the HOME directory
     cd [arg1] -> changes current working directory specified in [arg1]
       if [arg1] is not a valid directory, error message is printed.
+    ignores input arguments beyond arg1
   arg parsed_input: command struct, the parsed input from the user
   returns: void
 */
@@ -250,6 +307,86 @@ void execute_cd(struct command *parsed_input)
   // change directory
   chdir(dir);
   return;
+}
+
+/* 
+  Function execute_status()
+  ---------------------------------
+  description:
+    executes the status command
+    prints out either the exit status or the terminating signal of the last foreground process ran by your shell.
+      The three built-in shell commands do not count as foreground processes
+  returns: void
+*/
+void execute_status() {
+  /*
+  The status command prints out either the exit status or the terminating signal of the last foreground process ran by your shell.
+
+  If this command is run before any foreground command is run, then it should simply return the exit status 0.
+
+  The three built-in shell commands do not count as foreground processes for the purposes of this built-in command - i.e., status should ignore built-in commands.
+  */
+  printf("execute_status() called. This function is not yet implemented.");
+}
+
+/* 
+  Function exec_foreground()
+  ---------------------------------
+  description:
+    executes shell commands via exec() family of functions in the foreground.
+    waits for the command to complete before returning.
+  returns: void
+*/
+void exec_foreground(struct command *parsed_input) {
+  
+	int childStatus;
+	pid_t spawnPid = fork(); // fork new process
+
+	switch(spawnPid){
+	case -1:
+		perror("fork()\n");
+		exit(1);
+		break;
+	case 0:
+		// In the child process
+		// Replace the current program
+    execv(parsed_input->arguments[0], parsed_input->arguments);
+		// exec only returns if there is an error
+		perror("execve");
+		exit(2);
+		break;
+	default:
+		// In the parent process
+		// Wait for child's termination
+		spawnPid = waitpid(spawnPid, &childStatus, 0);
+		break;
+	}
+}
+
+/* 
+  Function exec_background()
+  ---------------------------------
+  description:
+    executes shell commands via exec() family of functions in the background
+    returns immediately, does not wait for command to complete
+  returns: void
+*/
+void exec_background(struct command *parsed_input) {
+  //TODO: IMPLEMENT
+}
+
+/* 
+  Function execute_exit()
+  ---------------------------------
+  description:
+    exits this shell program
+    kills all child processes or jobs before exiting.
+  returns: void
+*/
+void execute_exit() {
+  //TODO: IMPLEMENTE EXECUTE_EXIT() FUNCTION
+  printf("execute_exit() called. This function is not yet implemented.");
+
 }
 
 /* 
@@ -281,13 +418,31 @@ int execute(struct command *parsed_input)
 
   // hanlder for exit command
   else if (strcmp(parsed_input->full_text, "exit\n") == 0) {
-    // TODO: CREATE FUNCTION TO HANDLE EXIT FUNCTIONALITY
-    return 0;
+    exit(0);
   }
 
   // handler for cd command
   else if (strcmp(parsed_input->command, "cd") == 0){
     execute_cd(parsed_input);
+    return 1;
+  }
+
+  // handler for status command
+  else if (strcmp(parsed_input->full_text, "status\n") == 0){
+    execute_status();
+    return 1;
+  }
+
+  // handler for foreground commands
+  else if (parsed_input->background_process == false) {
+    exec_foreground(parsed_input);
+    return 1;
+  }
+
+  // handler for background commands
+  else if (parsed_input->background_process == true) {
+    exec_background(parsed_input);
+    return 1;
   }
   
 }
@@ -311,6 +466,9 @@ void small_shell()
     char *input = calloc(MAX_COMMAND_LEN+1, sizeof(char));
     char *expanded_input;
     struct command *user_command;
+
+    fflush(stdin);
+    fflush(stdout);
     
     printf_custom(": ", 0);
     fgets(input, MAX_COMMAND_LEN+1, stdin);
@@ -326,9 +484,24 @@ void small_shell()
   
 }
 
-/* execute the program
+/* 
+  Function killchildprocesses()
+  ---------------------------------
+  description:
+    kills all child processes and jobs of small shell before exiting
+  return: int
+*/
+void killchildprocesses()
+{
+  //TODO: IMPLEMENT
+  return;
+}
+
+/* 
+  execute the program
 */
 int main(void) {
+  //atexit(killchildprocesses);
   small_shell();
   return EXIT_SUCCESS;
 }
